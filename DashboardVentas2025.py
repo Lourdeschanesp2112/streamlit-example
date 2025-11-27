ad8b45003c0a3df7523bb9fd73bdc24501dc28da
Lourdeschanesp2112 <a18001339@alumnos.uady.mx>
2025-11-27 00:26:06 -0600

    Update DashboardVentas2025.py

--- DashboardVentas2025.py
+++ DashboardVentas2025.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly as px
 
 st.set_page_config(page_title='Análisis de Ventas y Ganancias', page_icon='📊', layout='wide')
 
